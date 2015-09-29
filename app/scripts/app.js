414a031aeb90f2d24271dc2e9285196c573900fa
Teri Pastorino <tpasto@comcast.net>
2015-09-29 10:43:32 -0700

    added angular scrip and html tag

---
+++ app/scripts/app.js
@@ -0,0 +1,6 @@
+//for refactoring later
+//blocJams = myAppModule in angular project instructions
+//blocJams = myApp
+
+//empty array used to inject dependencies
+var myAppModule = angular.module('myApp', []);
